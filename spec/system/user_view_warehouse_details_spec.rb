c7558bf9c0ef5368246e6af931f494c88b076ad3
felipel7 <felipelseven@gmail.com>
2023-04-19 17:19:20 -0300

    test(warehouse): add test for root redirect link

--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -22,4 +22,22 @@ describe "Warehouse details" do
     expect(page).to have_content("Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000")
     expect(page).to have_content("Galpão destinado para cargas internacionais")
   end
+
+  it "should have a link to redirect to the home page" do
+    Warehouse.create(
+      name: "Aeroporto SP",
+      code: "GRU",
+      city: "Guarulhos",
+      area: 100_000,
+      address: "Avenida do Aeroporto, 1000",
+      cep: "15000-000",
+      description: "Galpão destinado para cargas internacionais",
+    )
+
+    visit("/")
+    click_on("Aeroporto SP")
+    click_on("Voltar")
+
+    expect(current_path).to eq("/")
+  end
 end

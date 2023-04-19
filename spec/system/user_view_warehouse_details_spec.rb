bf773615be0483be74bdb6d55e57d30327a0de43
felipel7 <felipelseven@gmail.com>
2023-04-19 16:25:40 -0300

    test(warehouse): add test for displaying details

---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Warehouse details" do
+  it "should display warehouse details" do
+    Warehouse.create(
+      name: "Aeroporto SP",
+      code: "GRU",
+      city: "Guarulhos",
+      area: 100_000,
+      address: "Avenida do Aeroporto, 1000",
+      cep: "15000-000",
+      description: "Galpão destinado para cargas internacionais",
+    )
+
+    visit("/")
+    click_on("Aeroporto SP")
+
+    expect(page).to have_content("Galpão GRU")
+    expect(page).to have_content("Nome: Aeroporto SP")
+    expect(page).to have_content("Cidade: Guarulhos")
+    expect(page).to have_content("Área: 100.000 m²")
+    expect(page).to have_content("Endereço: Avenida do Aeroporto, 1000 CEP: 15000-000")
+    expect(page).to have_content("Galpão destinado para cargas internacionais")
+  end
+end

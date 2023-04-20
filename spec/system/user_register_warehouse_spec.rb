9b9cf74489526b43e0f0794d175f5fd9ec09fdaa
felipel7 <felipelseven@gmail.com>
2023-04-20 10:16:04 -0300

    test(warehouse): add test for create a new form

---
+++ spec/system/user_register_warehouse_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+describe "Warehouse form" do
+  it "should redirects user to create a new form when clicked" do
+    visit root_path
+    click_on "Cadastrar Galpão"
+
+    expect(page).to have_field("Nome")
+    expect(page).to have_field("Descrição")
+    expect(page).to have_field("Código")
+    expect(page).to have_field("Endereço")
+    expect(page).to have_field("Cidade")
+    expect(page).to have_field("CEP")
+    expect(page).to have_field("Área")
+  end
+
+  it "should submits the form successfully" do
+    visit root_path
+    click_on "Cadastrar Galpão"
+    fill_in "Nome", with: "Rio de Janeiro"
+    fill_in "Descrição", with: "Galpão da zona portuária do Rio"
+    fill_in "Código", with: "RIO"
+    fill_in "Endereço", with: "Avenida do Museu do Amanhã, 1000"
+    fill_in "Cidade", with: "Rio de Janeiro"
+    fill_in "CEP", with: "20100-000"
+    fill_in "Área", with: "32000"
+    click_on "Enviar"
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content "Rio de Janeiro"
+    expect(page).to have_content "RIO"
+    expect(page).to have_content "32.000 m²"
+  end
+end

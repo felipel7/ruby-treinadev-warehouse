89ef581e126598fc162f34fb90913215ae0a2d66
felipel7 <felipelseven@gmail.com>
2023-04-20 10:18:40 -0300

    feat(warehouse-form): add view, routes and actions

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,27 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    # params = hash
+    warehouse_params = params.require(:warehouse).permit( # Strong Parameters
+      :name,
+      :code,
+      :city,
+      :description,
+      :address,
+      :cep,
+      :area
+    )
+
+    warehouse = Warehouse.new(warehouse_params)
+
+    if warehouse.save
+      # There is no view here
+      redirect_to root_path
+    end
+  end
 end

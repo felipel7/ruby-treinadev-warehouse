a430aa769c28bd7f5992e946494b54a96905c28f
felipel7 <felipelseven@gmail.com>
2023-04-22 14:20:47 -0300

    feat(warehouse-destroy): add action and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: "home#index"
 
-  resources :warehouses, only: [:show, :new, :create, :edit, :update]
+  resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
 end

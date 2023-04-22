c96caaa5b8d38abbba904ed41d14c234aaad3548
felipel7 <felipelseven@gmail.com>
2023-04-21 23:39:37 -0300

    test(warehouse-edit-form): add tests for editing

---
+++ spec/system/user_edit_warehouse_spec.rb
@@ -0,0 +1,69 @@
+require "rails_helper"
+
+describe "Warehouse edit" do
+  it "should display details of a warehouse form when clicked" do
+    warehouse = Warehouse.create!(
+      name: "Maceio", code: "MCZ",
+      city: "Maceio", area: 50_000,
+      address: "Av. Main Street", cep: "300000-000",
+      description: "Galpão de Maceio",
+    )
+
+    visit root_path
+    click_on "Maceio"
+    click_on "Editar"
+
+    expect(page).to have_field("Nome", with: "Maceio")
+    expect(page).to have_field("Descrição", with: "Galpão de Maceio")
+    expect(page).to have_field("Código", with: "MCZ")
+    expect(page).to have_field("Endereço", with: "Av. Main Street")
+    expect(page).to have_field("Cidade", with: "Maceio")
+    expect(page).to have_field("CEP", with: "300000-000")
+    expect(page).to have_field("Área", with: 50_000)
+  end
+
+  it "should successfully update the warehouse information" do
+    warehouse = Warehouse.create!(
+      name: "Maceio", code: "MCZ",
+      city: "Maceio", area: 50_000,
+      address: "Av. Main Street", cep: "300000-000",
+      description: "Galpão de Maceio",
+    )
+
+    visit root_path
+    click_on "Maceio"
+    click_on "Editar"
+    fill_in "Nome", with: "Rio de Janeiro"
+    fill_in "Descrição", with: "Galpão da zona portuária do Rio"
+    fill_in "Código", with: "RIO"
+    fill_in "Endereço", with: "Avenida do Museu do Amanhã, 1000"
+    fill_in "Cidade", with: "Rio de Janeiro"
+    fill_in "CEP", with: "20100-000"
+    fill_in "Área", with: "32000"
+    click_on "Enviar"
+
+    expect(page).to have_content("Galpão atualizado com sucesso.")
+    expect(page).to have_content("Descrição: Galpão da zona portuária do Rio")
+    expect(page).to have_content("Código: RIO")
+    expect(page).to have_content("Cidade: Rio de Janeiro")
+  end
+
+  it "should prevent editing fields with empty values" do
+    warehouse = Warehouse.create!(
+      name: "Maceio", code: "MCZ",
+      city: "Maceio", area: 50_000,
+      address: "Av. Main Street", cep: "300000-000",
+      description: "Galpão de Maceio",
+    )
+
+    visit root_path
+    click_on "Maceio"
+    click_on "Editar"
+    fill_in "Nome", with: ""
+    fill_in "Descrição", with: ""
+    fill_in "Código", with: ""
+    click_on "Enviar"
+
+    expect(page).to have_content("Não foi possível atualizar o galpão.")
+  end
+end

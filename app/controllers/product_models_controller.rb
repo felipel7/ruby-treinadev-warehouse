32d0d48c9ee8c02bb7d44ca6c23372a2d993034b
felipel7 <felipelseven@gmail.com>
2023-04-25 09:50:44 -0300

    feat(product_model): add views, actions and routes

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -1,5 +1,40 @@
 class ProductModelsController < ApplicationController
   def index
-    @products = ProductModel.all
+    @product_models = ProductModel.all
+  end
+
+  def new
+    @product_model = ProductModel.new
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @product_model = ProductModel.new(product_model_params)
+
+    if @product_model.save
+      flash[:notice] = "Modelo de produto cadastrado com sucesso."
+      redirect_to @product_model # => show
+    else
+      flash[:alert] = "Modelo de produto não cadastrado."
+      render :new
+    end
+  end
+
+  def show
+    @product_model = ProductModel.find(params[:id])
+  end
+
+  private
+
+  def product_model_params
+    params.require(:product_model).permit(
+      :name,
+      :height,
+      :width,
+      :depth,
+      :weight,
+      :sku,
+      :supplier_id
+    )
   end
 end

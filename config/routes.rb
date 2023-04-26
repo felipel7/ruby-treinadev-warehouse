f4cf78d997daef4408710619bda137caaa94d5f8
felipel7 <felipelseven@gmail.com>
2023-04-26 12:21:07 -0300

    feat(order): add views, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
   resources :product_models, only: [:index, :show, :new, :create]
-
+  resources :orders, only: [:show, :new, :create]
   # authenticate :user do
   #   resources :product_models, only: [:index, :show, :new, :create]
   # end

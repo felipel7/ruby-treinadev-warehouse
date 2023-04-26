1f1a59aded29c6124dcc5d5573e819f948dbc42a
felipel7 <felipelseven@gmail.com>
2023-04-26 19:23:27 -0300

    feat(order): create a callback to generate code

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,19 @@ class Order < ApplicationRecord
   validates :estimated_delivery_date, presence: true
   validate :estimated_delivery_date_is_future
 
+  before_validation :generate_code
+
+  validates :code, :estimated_delivery_date, presence: true
+
+  private
+
   def estimated_delivery_date_is_future
     if self.estimated_delivery_date.present? && self.estimated_delivery_date <= Date.today
       self.errors.add(:estimated_delivery_date, " deve ser futura.")
     end
+  end
+
+  def generate_code
+    self.code = SecureRandom.alphanumeric(8).upcase
   end
 end

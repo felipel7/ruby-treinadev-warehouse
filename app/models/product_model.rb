0130c12db461d4c562ac04dad05da5e21a3a1ec4
felipel7 <felipelseven@gmail.com>
2023-04-27 11:56:32 -0300

    feat(product_model): add code validation

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,4 +1,15 @@
 class ProductModel < ApplicationRecord
   belongs_to :supplier
-  validates :name, :sku, presence: true
+  validates :name, :weight, :width, :height, :depth, :sku, presence: true
+  validates :sku, uniqueness: true, length: { maximum: 20 }
+  validate :check_dimension_values
+
+  private
+
+  def check_dimension_values
+    self.errors.add(:width, " deve ser maior que zero.") if self.width.to_i <= 0
+    self.errors.add(:height, " deve ser maior que zero.") if self.height.to_i <= 0
+    self.errors.add(:depth, " deve ser maior que zero.") if self.depth.to_i <= 0
+    self.errors.add(:weight, " deve ser maior que zero.") if self.weight.to_i <= 0
+  end
 end

f86a6561432e04f2ad1b9047d0a63b5ca6a77a0f
felipel7 <felipelseven@gmail.com>
2023-04-29 15:16:02 -0300

    test(stock_product): adding/removing stock items

--- spec/models/stock_product_spec.rb
+++ spec/models/stock_product_spec.rb
@@ -87,4 +87,84 @@ RSpec.describe StockProduct, type: :model do
       expect(original_serial).to eq stock_product.serial_number
     end
   end
+
+  describe "#available?" do
+    it "should be true if there is no destination" do
+      user = User.create!(name: "Maria", email: "maria@email.com", password: "123123")
+
+      warehouse = Warehouse.create!(
+        name: "Maceio", code: "MCZ",
+        city: "Maceio", area: 50_000,
+        address: "Av. Main Street", cep: "30000-000",
+        description: "Galpão de Maceio",
+      )
+
+      supplier = Supplier.create!(
+        corporate_name: "Samsung", brand_name: "Samsung Corporation",
+        registration_number: "12.222.222/2222-2",
+        full_address: "Av. Main Street, 123", city: "Curitiba",
+        state: "PR", email: "example@sams.com",
+      )
+
+      product = ProductModel.create!(
+        name: "TV 32 MODELO 1",
+        weight: 8_000,
+        width: 70,
+        height: 45,
+        depth: 10,
+        sku: "TV32-SAMSU-XPTO90",
+        supplier: supplier,
+      )
+
+      order = Order.create!(
+        user: user, warehouse: warehouse,
+        supplier: supplier, estimated_delivery_date: 1.week.from_now,
+      )
+
+      stock_product = StockProduct.create!(order: order, warehouse: warehouse, product_model: product)
+
+      expect(stock_product.available?).to eq true
+    end
+
+    it "should be false if there is already a destination" do
+      user = User.create!(name: "Maria", email: "maria@email.com", password: "123123")
+
+      warehouse = Warehouse.create!(
+        name: "Maceio", code: "MCZ",
+        city: "Maceio", area: 50_000,
+        address: "Av. Main Street", cep: "30000-000",
+        description: "Galpão de Maceio",
+      )
+
+      supplier = Supplier.create!(
+        corporate_name: "Samsung", brand_name: "Samsung Corporation",
+        registration_number: "12.222.222/2222-2",
+        full_address: "Av. Main Street, 123", city: "Curitiba",
+        state: "PR", email: "example@sams.com",
+      )
+
+      product = ProductModel.create!(
+        name: "TV 32 MODELO 1",
+        weight: 8_000,
+        width: 70,
+        height: 45,
+        depth: 10,
+        sku: "TV32-SAMSU-XPTO90",
+        supplier: supplier,
+      )
+
+      order = Order.create!(
+        user: user, warehouse: warehouse,
+        supplier: supplier, estimated_delivery_date: 1.week.from_now,
+      )
+
+      stock_product = StockProduct.create!(order: order, warehouse: warehouse, product_model: product)
+      stock_product.create_stock_product_destination!(
+        recipient: "Maria",
+        address: "Rua da Maria",
+      )
+
+      expect(stock_product.available?).to eq false
+    end
+  end
 end
--- spec/system/stock_product/user_view_stock_from_warehouse_spec.rb
+++ spec/system/stock_product/user_view_stock_from_warehouse_spec.rb
@@ -60,9 +60,58 @@ describe "Stock details" do
     visit root_path
     click_on "Maceio"
 
-    expect(page).to have_content "Itens em Estoque"
-    expect(page).to have_content "3 x TV32-SAMSU-XPTO90"
-    expect(page).to have_content "2 x SOU71-SAMSU-NOIZ77"
-    expect(page).not_to have_content "TV42-SAMSU-XPXO20"
+    within "#stock_products" do
+      expect(page).to have_content "Itens em Estoque"
+      expect(page).to have_content "3 x TV32-SAMSU-XPTO90"
+      expect(page).to have_content "2 x SOU71-SAMSU-NOIZ77"
+      expect(page).not_to have_content "TV42-SAMSU-XPXO20"
+    end
+  end
+
+  it "should be able to remove an item from stock" do
+    user = User.create!(name: "Maria", email: "maria@email.com", password: "123123")
+
+    warehouse = Warehouse.create!(
+      name: "Maceio", code: "MCZ",
+      city: "Maceio", area: 50_000,
+      address: "Av. Main Street", cep: "30000-000",
+      description: "Galpão de Maceio",
+    )
+
+    supplier = Supplier.create!(
+      corporate_name: "Samsung", brand_name: "Samsung Corporation",
+      registration_number: "22.222.222/2222-2",
+      full_address: "Av. Main Street, 123", city: "Curitiba",
+      state: "PR", email: "example@acme.com",
+    )
+
+    product = ProductModel.create!(
+      name: "TV 32",
+      weight: 8_000,
+      width: 70,
+      height: 45,
+      depth: 10,
+      sku: "TV32-SAMSU-XPTO90",
+      supplier: supplier,
+    )
+
+    order = Order.create!(
+      user: user, warehouse: warehouse,
+      supplier: supplier, estimated_delivery_date: 1.week.from_now,
+    )
+
+    2.times { StockProduct.create!(order: order, warehouse: warehouse, product_model: product) }
+
+    login_as(user)
+    visit root_path
+    click_on "Maceio"
+    select "TV32-SAMSU-XPTO90", from: "Item para Saída"
+    fill_in "Destinatário", with: "Maria Ferreira"
+    fill_in "Endereço Destino", with: "Rua das Palmeiras, 100 - Campinas - São Paulo"
+    click_on "Confirmar Retirada"
+
+    expect(current_path).to eq warehouse_path(warehouse.id)
+    expect(page).to have_content "Item retirado com sucesso"
+    expect(page).to have_content "1 x TV32-SAMSU-XPTO90"
   end
 end

12fd5e917f83192ad7213efde862e6cb2856679b
felipel7 <felipelseven@gmail.com>
2023-04-29 13:37:45 -0300

    feat: display stock on warehouse details page

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -3,6 +3,7 @@ class Warehouse < ApplicationRecord
   validates :code, length: { is: 3 }
   validates :name, :code, uniqueness: true
   validates :cep, format: { with: /\A\d{5}-\d{3}\z/, message: " deve ser neste formato: 00000-000" }
+  has_many :stock_products
 
   def full_description
     "#{code} - #{name}"

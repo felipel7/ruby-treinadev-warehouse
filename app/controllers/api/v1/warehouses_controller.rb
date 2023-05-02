f6710ba2737d309c7e5897a6754df572c1fcf754
felipel7 <felipelseven@gmail.com>
2023-05-01 22:07:35 -0300

    feat(api): add endpoint to retrieve all warehouses

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::WarehousesController < ActionController::API
+  def index
+    # warehouses = Warehouse.all.order(:name)
+    warehouses = Warehouse.all
+    render status: 200, json: warehouses.as_json(except: [:created_at, :updated_at])
+  end
+
   def show
-    warehouse = Warehouse.find(params[:id])
-    render status: 200, json: warehouse
+    # warehouse = Warehouse.find_by(id: params[:id]) => retorna nil, caso nao exista
+
+    # if warehouse.nil?
+    #   render status: 404
+    # else
+    #   render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+    # end
+
+    begin
+      warehouse = Warehouse.find(params[:id])
+      render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+    rescue => exception
+      render status: 404
+    end
   end
 end

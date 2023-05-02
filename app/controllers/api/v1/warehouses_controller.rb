a5633c428a6c75a63cb8335f1eab0e28df99ed0c
felipel7 <felipelseven@gmail.com>
2023-05-01 23:52:08 -0300

    feat(api): add endpoint to post a warehouse

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::WarehousesController < ActionController::API
+class Api::V1::WarehousesController < Api::V1::ApiController
   def index
     # warehouses = Warehouse.all.order(:name)
     warehouses = Warehouse.all
@@ -14,11 +14,34 @@ class Api::V1::WarehousesController < ActionController::API
     #   render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
     # end
 
-    begin
-      warehouse = Warehouse.find(params[:id])
-      render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
-    rescue => exception
-      render status: 404
+    # begin
+    #   warehouse = Warehouse.find(params[:id])
+    #   render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+    # rescue => exception
+    #   render status: 404
+    # end
+
+    warehouse = Warehouse.find(params[:id])
+    render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(
+      :name,
+      :code,
+      :city,
+      :area,
+      :address,
+      :cep,
+      :description
+    )
+
+    warehouse = Warehouse.new(warehouse_params)
+
+    if warehouse.save
+      render status: 201, json: warehouse
+    else
+      render status: 412, json: { errors: warehouse.errors.full_messages }
     end
   end
 end
